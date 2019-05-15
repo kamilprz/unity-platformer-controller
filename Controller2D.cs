e78a66469c75292ae09c3beed0bd52ee0d4e0c05
kamilprz <36544756+kamilprz@users.noreply.github.com>
2019-05-15 12:44:15 +0100

    scuffed walljumping

--- Controller2D.cs
+++ Controller2D.cs
@@ -13,7 +13,7 @@ public class Controller2D : RaycastController
         base.Start();
     }
 
-    public void Move(Vector3 velocity)
+    public void Move(Vector3 velocity, bool standingOnPlatform = false)
     {
         UpdateRaycastOrigins();
         collisions.Reset();
@@ -32,6 +32,10 @@ public class Controller2D : RaycastController
         }
 
         transform.Translate(velocity);
+        if (standingOnPlatform)
+        {
+            collisions.below = true;
+        }
     }
 
     void HorizontalCollisions(ref Vector3 velocity)
@@ -49,6 +53,11 @@ public class Controller2D : RaycastController
 
             if (hit)
             {
+                if(hit.distance == 0)
+                {
+                    continue;
+                }
+
                 float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
 
                 if (i == 0 && slopeAngle < maxClimbAngle) {
--- Player.cs
+++ Player.cs
@@ -12,6 +12,14 @@ public class Player : MonoBehaviour
     public float accelerationTimeAirborne = .2f;
     public float accelerationTimeGrounded = .1f;
 
+    public float wallSlideSpeedMax = 3;
+    public float wallStickTime = .25f;
+    float timeToWallUnstick;
+
+    public Vector2 wallJumpClimb;
+    public Vector2 wallJumpOff;
+    public Vector2 wallLeap;
+
     bool doubleJump;
     bool dashed;
     float gravity;
@@ -32,23 +40,79 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        int wallDirX = (controller.collisions.left) ? -1 : 1;
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+
+        bool wallSliding = false;
+        if((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0)
+        {
+            //print("velocity X = " + velocity.x);
+            wallSliding = true;
+            if(velocity.y < -wallSlideSpeedMax)
+            {
+                velocity.y = -wallSlideSpeedMax;
+            }
+
+            if(timeToWallUnstick > 0)
+            {
+                velocityXSmoothing = 0;
+                velocity.x = 0;
+                if(input.x != wallDirX && input.x != 0)
+                {
+                    timeToWallUnstick -= Time.deltaTime;
+                }
+                else
+                {
+                    timeToWallUnstick = wallStickTime;
+                }
+            }
+            else
+            {
+                timeToWallUnstick = wallStickTime;
+            }
+        }
+
         if(controller.collisions.above || controller.collisions.below)
         {
             velocity.y = 0;
         }
 
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if(controller.collisions.below){
-                velocity.y = jumpVelocity;
-                doubleJump = true;
-            }
-            else if (doubleJump)
+            if (wallSliding)
             {
-                doubleJump = false;
-                velocity.y = jumpVelocity;
+                if(wallDirX == input.x)
+                {
+                    velocity.x = -wallDirX * wallJumpClimb.x;
+                    velocity.y = wallJumpClimb.y;
+                }
+                else if(input.x == 0)
+                {
+                    velocity.x = -wallDirX * wallJumpOff.x;
+                    velocity.y = wallJumpOff.y;
+                }
+                else
+                {
+                    velocity.x = -wallDirX * wallLeap.x;
+                    velocity.y = wallLeap.y;
+                }
+            }
+            else
+            {
+                if (controller.collisions.below)
+                {
+                    velocity.y = jumpVelocity;
+                    doubleJump = true;
+                }
+                else if (doubleJump)
+                {
+                    doubleJump = false;
+                    velocity.y = jumpVelocity;
+                }
             }
         }
 
@@ -57,38 +121,13 @@ public class Player : MonoBehaviour
             if (!controller.collisions.below)
             {
                 // add boolean so can only do it once
+                wallSliding = false;
                 velocity.y = -1.5f * jumpVelocity;
-                print("dropped DOWN");
 
             }
         }
 
-
-
-        float targetVelocityX = input.x * moveSpeed;
-        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded : accelerationTimeAirborne);
-
-        /*
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            if (controller.collisions.below)
-            {
-                dashed = false;
-            }
-            if (!dashed)
-            {
-                velocity.y = 1;
-                velocity.x = input.x * moveSpeed * dashDistance;
-                dashed = true;
-                print("dashed SIDE");
-            }
-        } 
-        */
-
         velocity.y += gravity * Time.deltaTime;
-
- 
-
         controller.Move(velocity * Time.deltaTime);
     }
 }

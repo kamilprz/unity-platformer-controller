aa55a00b80ff79c03db2ce1767460113db88bee0
kamilprz <kamil.prz@gmail.com>
2019-05-15 19:55:08 +0100

    uploaded project folder
    added dashing (limited to 1 in air)
    doesn't seem to fall of moving platform when passenger moving

---
+++ Raycast Movement/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Player))]
+public class PlayerInput : MonoBehaviour
+{
+
+    Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Player>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        player.SetDirectionalInput(directionalInput);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.Jump();
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            player.FastFall();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            player.Dash();
+        }
+    }
+}
